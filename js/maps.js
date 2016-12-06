2bc29ebfebd5b3282ae22a136ff582e0a1074e21
Andrew Rottier <andrewrottier95@gmail.com>
2016-12-06 16:53:03 -0500

    profile

--- js/maps.js
+++ js/maps.js
@@ -1,4 +1,8 @@
 
+ var infowindow;
+         // content: contentString
+        //});; //used to display info about the markets, we only want 1
+
 
 function searchFor() {
   //Get value from search box
@@ -31,19 +35,19 @@ function searchFor() {
 
       //Handle result from geocoding
   function reqListener () {
-  oReq.open("POST", "/searchMovies", true);
-  // MAJOR KEY-- build form in XML http request SEND function
-  console.log('search='+convertedLocation);
+  	oReq.open("POST", "/searchMovies", true);
+  	// MAJOR KEY-- build form in XML http request SEND function
+  	console.log('search='+convertedLocation);
 
-  //THIS ISSNT SENDING THE RESQUEST PROPERLY
-  oReq.send('search='+convertedLocation);
+  	//THIS ISSNT SENDING THE RESQUEST PROPERLY
+  	oReq.send('search='+convertedLocation);
     oReq.addEventListener("load", receivedResponse); //Callback to next function
   }
 
   //Handle the server response
   function receivedResponse(){
-  //console.log(" req.responseText: "+oReq.response);
-  popMap( JSON.parse(oReq.response) );
+  	//console.log(" req.responseText: "+oReq.response);
+  	popMap( JSON.parse(oReq.response) );
   }
 
 }
@@ -62,9 +66,7 @@ scrollwheel: false,
 styles: [{"featureType":"landscape","stylers":[{"hue":"#FFA800"},{"saturation":0},{"lightness":0},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#53FF00"},{"saturation":-73},{"lightness":40},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FBFF00"},{"saturation":0},{"lightness":0},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#00FFFD"},{"saturation":0},{"lightness":30},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#00BFFF"},{"saturation":6},{"lightness":8},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#679714"},{"saturation":33.4},{"lightness":-25.4},{"gamma":1}]}]
 });
 
-
-
-var infoWindow = new google.maps.InfoWindow({map: map});
+//var infoWindow = new google.maps.InfoWindow({map: map});
 
         // Geolocation services, start user at current geolocation
         /*if (navigator.geolocation) {
@@ -126,11 +128,15 @@ function popMap(foundMarkets){
             map: map,
             title: foundMarkets.MarketName
           });
+          //store the ID of the farm in storage incase users want to load page 
+          localStorage.setItem("storageName",foundMarkets.FMID);
+
           var contentString =
           '<div id="MarkerContent">'+
             '<div id="siteNotice">'+
             '</div>'+
-            '<h4 id="firstHeading" class="firstHeading"><a href="'+foundMarkets.Website+'">'+ foundMarkets.MarketName+'</a></h4>'+
+            '<h4 id="firstHeading" class="firstHeading"><a href="/html/profile.html">'+ foundMarkets.MarketName+'</a></h4>'+
+            //'<h4 id="firstHeading" class="firstHeading"><a href="'+foundMarkets.Website+'">'+ foundMarkets.MarketName+'</a></h4>'+    Old, link to their website
             '<div id="bodyContent">'+
             '<p><b>State: </b>'+ foundMarkets.State+' <b>City:</b> '+ foundMarkets.city+' </p>'+
             '<p><b>Season: </b>'+ foundMarkets.Season1Date+' <b>Time:</b> '+ foundMarkets.Season1Time+' </p>'+
@@ -140,12 +146,14 @@ function popMap(foundMarkets){
         '</div>'+
        '</div>';
 
-        var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
-
+        //infowindow = new google.maps.InfoWindow({
+         // content: contentString
+        //});
+        infowindow = new google.maps.InfoWindow({});
         marker.addListener('click', function() {
-          infowindow.open(map, marker);
+        	infowindow.setContent(contentString);
+   			//infowindow.open(map, this);
+        	infowindow.open(map, marker);
         });
         }
 
--- js/server.js
+++ js/server.js
@@ -28,7 +28,7 @@ var farms = [{name: "farmtest", lat: 100, lon: 100}];
 //When we click the search bar
 app.post('/searchMovies', function(req, res){
   var body = ''
-  console.log("Handling Search");
+  //console.log("Handling Search");
   req.on('data', function(d) {
     body += d;
   })
@@ -36,13 +36,13 @@ app.post('/searchMovies', function(req, res){
       var post = qs.parse( body )
 
     ///Convert char array to lat and long
-    console.log("Search for: "+ post.search);
+    //console.log("Search for: "+ post.search);
     var locArray = post.search.replace(/[()]/gi, '').split(',');
 
     var lati = locArray[0];
     var long = locArray[1];
-    console.log("Lat: "+ lati + ", Lon: "+ long);
-    console.log("************************");
+    //console.log("Lat: "+ lati + ", Lon: "+ long);
+    //console.log("************************");
     //calculate the four corners to use later
     var top = (Math.round(100*lati)/100) +.15; 
     //var top = Math.ceil( lati );
@@ -53,10 +53,10 @@ app.post('/searchMovies', function(req, res){
 
     //var right = Math.floor( long );
     var right = (Math.round(100*long)/100) -.15; 
-    console.log("Top : " + top);
-    console.log("Bottom : " + bottom);
-    console.log("Left : " + left); //maybe?
-    console.log("Right : " + right);
+    //console.log("Top : " + top);
+    //console.log("Bottom : " + bottom);
+    //console.log("Left : " + left); //maybe?
+    //console.log("Right : " + right);
 
     //Chopped good!
 
@@ -69,7 +69,7 @@ app.post('/searchMovies', function(req, res){
                   
       //Create a promise here
       var p1 = new Promise(function(resolve, reject) {
-        console.log("Inside Promise");
+        //console.log("Inside Promise");
        
         var count = 0;
         //Query the areas in the square
@@ -83,7 +83,7 @@ app.post('/searchMovies', function(req, res){
             //Naming of the variables pulled from the DB
             //farms.push({FMID: item.FMID, MarketName: item.MarketName, Website: item.Website, Facebook: item.Facebook, Twitter: item.Twitter, Youtube: item.Youtube, street: item.street, city: item.city, County: item.County, State: item.State, zip: item.zip, Season1Date: item.Season1Date, Season1Time: item.Season1Time, Credit: item.Credit, Organic: item.Organic, Eggs: item.Eggs, Vegetables: item.Vegetables, lat: item.y, lon: item.x, });
             farms.push({FMID: item.FMID, MarketName: item.MarketName, Website: item.Website, Facebook: item.Facebook, Twitter: item.Twitter, Youtube: item.Youtube, OtherMedia: item.OtherMedia, street: item.street, city: item.city, County: item.County, State: item.State, zip: item.zip, Season1Date: item.Season1Date, Season1Time: item.Season1Time, Season2Date: item.Season2Date, Season2Time: item.Season2Time, Season3Date: item.Season3Date, Season3Time: item.Season3Time, Season4Date: item.Season4Date, Season4Time: item.Season4Time, x: item.x, y: item.y, Location: item.Location, Credit: item.Credit, WIC: item.WIC, WICcash: item.WICcash, SFMNP: item.SFMNP, SNAP: item.SNAP, Organic: item.Organic, Bakedgoods: item.Bakedgoods, Cheese: item.Cheese, Crafts: item.Crafts, Flowers: item.Flowers, Eggs: item.Eggs, Seafood: item.Seafood, Herbs: item.Herbs, Vegetables: item.Vegetables, Honey: item.Honey, Jams: item.Jams, Maple: item.Maple, Meat: item.Meat, Nursery: item.Nursery, Nuts: item.Nuts, Plants: item.Plants, Poultry: item.Poultry, Prepared: item.Prepared, Soap: item.Soap, Trees: item.Trees, Wine: item.Wine, Coffee: item.Coffee, Beans: item.Beans, Fruits: item.Fruits, Grains: item.Grains, Juices: item.Juices, Mushrooms: item.Mushrooms, PetFood: item.PetFood, Tofu: item.Tofu, WildHarvested: item.WildHarvested, updateTime: item.updateTime});
-            console.log(item.MarketName + ", Lat: " + item.y + ", Lon: " + item.x);
+            //console.log(item.MarketName + ", Lat: " + item.y + ", Lon: " + item.x);
             count ++;
           }
 
@@ -94,7 +94,7 @@ app.post('/searchMovies', function(req, res){
             myCursor.toArray(function(err, items) {
                             
             //resolve when we get to the end of the query
-            console.log("array length (INSIDE PROMISE): "+ farms.length);
+            //console.log("array length (INSIDE PROMISE): "+ farms.length);
             
             resolve(farms);
             //db.close();
@@ -105,7 +105,7 @@ app.post('/searchMovies', function(req, res){
     });
 
     p1.then(function(value) {
-      console.log("Number of found markets: "+value.length); // Success!
+      //console.log("Number of found markets: "+value.length); // Success!
       res.end(JSON.stringify(value)   );
 
     }, function(reason) {
@@ -158,7 +158,7 @@ app.get('/getStateCount', function(req, res){
         }
         else { //not found yet
           myMap.set(item.State, 1); //set count to 1
-          console.log(item.State); //All unique states
+          //console.log(item.State); //All unique states
         }
       }
       // If the item is null then the cursor is exhausted/empty and closed
@@ -166,7 +166,7 @@ app.get('/getStateCount', function(req, res){
                     
       // Show that the cursor is closed
       myCursor.toArray(function(err, items) {
-        console.log("Count: "+count);
+        //console.log("Count: "+count);
         resolve(myMap);
         // Let's close the db
         //db.close();
@@ -179,7 +179,7 @@ app.get('/getStateCount', function(req, res){
   p1.then(function(myMap) {
     //loop through hashmap and display counts
     for (var [key, value] of myMap) {
-      console.log("States: " + key + ", "+value);
+      //console.log("States: " + key + ", "+value);
     }
     //send back data
     res.end(JSON.stringify(Array.from(myMap)) );
@@ -195,6 +195,73 @@ app.get('/getStateCount', function(req, res){
 }
 )
 });
+
+
+
+
+app.get('/getFarm', function(req, res){
+  var body = ''
+  console.log("Handling Search for Farm ID");
+  req.on('data', function(d) {
+    body += d;
+  })
+  req.on('end', function(d) {
+    var post = qs.parse( body )
+
+    ///Convert char array to lat and long
+    //console.log("Post: "+ post);
+    console.log("Search for farm: "+ post.search);
+
+    var farmID = parseInt(post.search);
+
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+    console.log('Connection established to', url);
+
+    //get the name of the collection from the database
+    var collection = db.collection('markets');
+              
+    //Create a promise here
+    var p1 = new Promise(function(resolve, reject) {
+    //Find farm
+    var myCursor = db.collection('markets').find({FMID: farmID});
+    
+    // Execute the each command, triggers for each document
+    myCursor.each(function(err, item) {
+      // If the item is null then the cursor is exhausted/empty and closed
+      if(item == null) {
+                    
+      // Show that the cursor is closed
+      myCursor.toArray(function(err, items) {
+        resolve(item);
+        // Let's close the db
+        //db.close();
+      });
+    };
+  });          
+   // reject ("Error!");
+  });
+
+  p1.then(function(farm) {
+ 
+    //send back data
+    res.end(JSON.stringify(Array.from(farm)) );
+
+
+    //console.log("Number of found markets: "+value); // Success!
+  }, function(reason) {
+    console.log("fail: "+reason); // Error!
+  });
+  }
+
+});
+}
+)
+});
+
 
 
 
