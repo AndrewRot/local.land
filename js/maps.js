1af82fdd9bf8de51cb8e19a53f5143ea800f2036
Andrew Rottier <andrewrottier95@gmail.com>
2016-12-13 17:23:44 -0500

    Relocation

--- js/maps.js
+++ js/maps.js
@@ -89,17 +89,19 @@ styles: [{"featureType":"landscape","stylers":[{"hue":"#FFA800"},{"saturation":0
 
 }
 
-
+//Switch this to take a param as a location - then pass it on search
+//also send fm.Address to it from slidebar clicks
 //Handle relocating map when user searchers
-function codeAddress() {
+function codeAddress(address) {
 console.log(document.getElementById('ZippyZip').value);
-    var address = document.getElementById('ZippyZip').value;
+    //var address = document.getElementById('ZippyZip').value;
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == 'OK') {
         map.setCenter(results[0].geometry.location);
         var marker = new google.maps.Marker({
             map: map,
-            position: results[0].geometry.location
+            position: results[0].geometry.location,
+             zIndex:999999
         });
 
       } else {
@@ -111,95 +113,118 @@ console.log(document.getElementById('ZippyZip').value);
 
 function popMap(foundMarkets){
 
-	//var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-        var icons = {
-          beer: { icon: 'images/beerimg.png' },
-          farm: { icon: 'images/logo.png' }
-        };
+  var icons = {
+    beer: { icon: 'images/beerimg.png' },
+    farm: { icon: 'images/logo.png' }
+  };
 
-        function addMarker(foundMarkets) {
+  function addMarker(foundMarkets) {
 
-        //get position
-        var position =  new google.maps.LatLng(foundMarkets.y, foundMarkets.x);
+    //get position
+    var position =  new google.maps.LatLng(foundMarkets.y, foundMarkets.x);
 
-          //switch to datapassed from DB
-          var marker = new google.maps.Marker({
-            position: position,
-            icon: icons['farm'].icon,
-            map: map,
-            title: foundMarkets.MarketName
-          });
-          //store the ID of the farm in storage incase users want to load page 
-          localStorage.setItem("storageName",foundMarkets.FMID);
+    //switch to datapassed from DB
+    var marker = new google.maps.Marker({
+      position: position,
+      icon: icons['farm'].icon,
+      map: map,
+      title: foundMarkets.MarketName
+    });
+    //store the ID of the farm in storage incase users want to load page 
+    //localStorage.setItem("storageName",foundMarkets.FMID);
 
 
-          var headerString = '<h4 id="firstHeading" class="firstHeading"><a href="'+foundMarkets.Website+'"><b>'+ foundMarkets.MarketName+'</b></a></h4>'   /// Old, link to their website
+    var headerString = '<h4 id="firstHeading" class="firstHeading"><a href="'+foundMarkets.Website+'"><b>'+ foundMarkets.MarketName+'</b></a></h4>'   /// Old, link to their website
+    var contentString =
+      '<div id="MarkerContent">'+
+      '<div id="bodyContent">'+
+      '<p><b>State: </b>'+ foundMarkets.State+' <b>City:</b> '+ foundMarkets.city+ ' <b>Zip:</b> '+ foundMarkets.zip+ ' <b>Street:</b> '+ foundMarkets.street+' </p>'+
+      '<p><b>Season: </b>'+ foundMarkets.Season1Date+' <b>Time:</b> '+ foundMarkets.Season1Time+' </p>'+
+      '<div id="foodicons style= "display: "inline-flex"">'+
+      getIcons(foundMarkets);
+      '</div> </div> </div>';
+    var footerString = 
+      '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
+      '<button type="button" class="btn btn-primary">Save Market</button>';
 
-          var contentString =
-          '<div id="MarkerContent">'+
-            //'<div id="siteNotice">'+
-            //'<h4 id="firstHeading" class="firstHeading"><a href="/html/profile.html">'+ foundMarkets.MarketName+'</a></h4>'+      //Link to profile
-            //'</div>'+
-            //'<div class="marketPage"><h4 id="firstHeading" class="firstHeading" >'+ foundMarkets.MarketName+'</h4></div>'+     ///Slide out attempt
-            //'<h4 id="firstHeading" class="firstHeading"><a href="'+foundMarkets.Website+'">'+ foundMarkets.MarketName+'</a></h4>'+   /// Old, link to their website
-            '<div id="bodyContent">'+
-            '<p><b>State: </b>'+ foundMarkets.State+' <b>City:</b> '+ foundMarkets.city+ ' <b>Zip:</b> '+ foundMarkets.zip+ ' <b>Street:</b> '+ foundMarkets.street+' </p>'+
-            '<p><b>Season: </b>'+ foundMarkets.Season1Date+' <b>Time:</b> '+ foundMarkets.Season1Time+' </p>'+
-           '<div id="foodicons style= "display: "inline-flex"">'+
-           getIcons(foundMarkets);
-           '</div>'+
-        '</div>'+
-       '</div>';
-       var footerString = 
-       	//'<div class="fb-follow" data-href="'+foundMarkets.Facebook+'" data-layout="standard" data-size="large" data-show-faces="true">TEST</div>' +
-	       //'<div id="leftFooter"  style= "float: left>"'+
-	       	
-	       //'</div>'+
-	       //'<div id="rightFooter" style= "float: right">'+
-		       '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
-		       '<button type="button" class="btn btn-primary">Save Market</button>';
-	       //'</div>';
 
-        //infowindow = new google.maps.InfoWindow({
-         // content: contentString
-        //});
-        infowindow = new google.maps.InfoWindow({});
-        marker.addListener('click', function() {
-        	console.log("Clicked location");
+    //infowindow = new google.maps.InfoWindow({
+    // content: contentString
+    //});
+    infowindow = new google.maps.InfoWindow({});
+    marker.addListener('click', function() {
+    	console.log("Clicked location");
 
-        	updateURL(foundMarkets.Facebook);
-        	//Infor window is the google api built in tag
-        	//infowindow.setContent(contentString);
-        	//infowindow.open(map, marker);
-        	//Open modal
-        	modal.style.display = "block";
+    	updateURL(foundMarkets.Facebook);
+    	//Infor window is the google api built in tag
+    	//infowindow.setContent(contentString);
+    	//infowindow.open(map, marker);
+    	//Open modal
+    	modal.style.display = "block";
+    	modaltitle.innerHTML = headerString;
+    	modalbody.innerHTML = contentString;
+    	modalfooter.innerHTML = footerString;
+    });
 
-        	modaltitle.innerHTML = headerString;
-        	modalbody.innerHTML = contentString;
-        	modalfooter.innerHTML = footerString;
-        	
-        });
+  
+
+    //add content to the side bar and also link it with the marker
+    //populate the side bar with top choices (sliderContent)
+    var sc = document.getElementById('sliderContent');
+      function slidePanel(fm, marker){
+        //if undefined, dont include
+        //console.log("web: " + fm.Website);
+        if(fm.Website === "undefined" || fm.MarketName === "undefined"){
+          console.log("UNDEFINED");
+          return '';
         }
+        else{
+       
+        //var html = '<li><a href="'+fm.Website+'">'+fm.MarketName+'</a></li>';
+        var html = '<li><div id="slidertag" onclick="codeAddress(\''+fm.street+', '+fm.city +'\'); ">'+fm.MarketName+'</div></li>';
+        return html;
+      }
+    
+    }
 
-        for (var i = 0; i < foundMarkets.length; i++) {
-          addMarker(foundMarkets[i]);
+    sc.innerHTML += slidePanel(foundMarkets, marker);
+    var locater = document.getElementById('slidertag');
+        locater.onClick = function () {
+          updateMarker(marker) ;
+          console.log("clicked!!!!!!");
         }
+    }
+    //update the marker - how can we call this?
+    function updateMarker(m){
+      m.setIcon('images/selectedlogo.png');
+    }
 
-        //populate the side bar with top choices (sliderContent)
-        function slidePanel(fm){
-          var html = '<li><a href="'+fm.Website+'">'+fm.MarketName+'</a></li>'
-          return html;
-        }
 
-        var sliderContent = '';
-        var sc = document.getElementById('sliderContent');
-        for (var i = 0; i < foundMarkets.length; i++) {
-          sliderContent = sliderContent + slidePanel(foundMarkets[i]);
-          
-        }
-        sc.innerHTML = ""; //clear out old content
-        
-        sc.innerHTML = sliderContent;
+
+
+    var sliderContent = '';
+    var sc = document.getElementById('sliderContent');
+    sc.innerHTML = "";
+    //dump(sc.innerHTML);
+    sc.innerHTML = '<li><a href="#"><b>Markets</b></a></li>';
+
+    //Add all of the markers to the map
+    //sc.innerHTML = "";
+    for (var i = 1; i < foundMarkets.length; i++) {
+      addMarker(foundMarkets[i]);
+    }
+
+  
+
+  
+  //first is always undefined?
+  //for (var i = 1; i < foundMarkets.length; i++) {
+    //sliderContent = sliderContent + slidePanel(foundMarkets[i]);
+    
+  //}
+  //sc.innerHTML = ""; //clear out old content
+  //document.getElementById('sliderContent').innerHTML = "";
+  //sc.innerHTML = sliderContent;
 
 
 
